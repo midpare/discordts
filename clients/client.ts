38a7dd98658e207003a6c7956206aa9e00dde4d5
midpare <midpare@gmail.com>
2021-12-20 21:35:49 +0900

    update err

--- clients/client.ts
+++ clients/client.ts
@@ -1,5 +1,4 @@
 import { Client, Collection } from 'discord.js'
-import { CommandType } from '../typings/command'
 
 const client: any = new Client({ intents: 32767 })
 
@@ -9,16 +8,6 @@ client.subCommands = new Collection()
 client.subAliases = new Collection()
 client.coin = new Collection()
 client.interactions = new Collection()
-// client.coin = new Collection()
+client.coin = new Collection()
 
 export default client
-
-
-class testClient extends Client {
-  commands: Collection<string, CommandType> = new Collection()
-
-  constructor() {
-    super({ intents: 32767 })
-  }
-  
-}

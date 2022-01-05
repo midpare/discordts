2ae9abf33ff6448f859ccee5ebe679c13f73683e
midpare <midpare@gmail.com>
2022-01-05 22:43:47 +0900

    delete dist

--- dist/structures/Interaction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Interaction = void 0;
-class Interaction {
-    constructor(options) {
-        Object.assign(this, options);
-    }
-}
-exports.Interaction = Interaction;

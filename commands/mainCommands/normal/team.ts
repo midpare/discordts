3c8368260f9b2f3d4ae285d2fd9dee1f592c4223
midpare <midpare@gmail.com>
2022-01-07 12:52:11 +0900

    Update team.ts

--- commands/mainCommands/normal/team.ts
+++ commands/mainCommands/normal/team.ts
@@ -9,19 +9,19 @@ export default new Command({
   usage: '팀 <이름> <이름> ...',
   description: '적은 <이름>만큼의 유저를 1팀과 2팀으로 나눕니다.',
   execute: async ({ msg, args }) => {
-    const embed = new MessageEmbed();
     if (!args[0])
       return msg.reply('기입할 이름을 입력해주세요');
-    const length = args.length;
-    const shuffleArgs = shuffle(args);
+
     const team1 = new Array();
     const team2 = new Array();
-    for (let i = 0; i < length; i += 2) {
+
+    const shuffleArgs = shuffle(args);
+    for (let i = 0; i < args.length; i += 2) {
       team1.push(shuffleArgs[i]);
       team2.push(shuffleArgs[i + 1]);
     }
 
-    embed
+    const embed = new MessageEmbed()
       .setTitle('팀')
       .addFields(
         { name: '1팀', value: `${team1.join(', ')}`, inline: false},

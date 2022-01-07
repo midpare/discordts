8c484ada7fdb358d3dffcbd200f320e1c4fbb18a
midpare <midpare@gmail.com>
2022-01-07 12:44:47 +0900

    update team

--- commands/mainCommands/normal/team.ts
+++ commands/mainCommands/normal/team.ts
@@ -5,22 +5,28 @@ import { shuffle } from '../../../structures/Util';
 export default new Command({
   name: '팀',
   aliases: ['팀'],
-  category: '게임',
+  category: '기본',
   usage: '팀 <이름> <이름> ...',
   description: '적은 <이름>만큼의 유저를 1팀과 2팀으로 나눕니다.',
   execute: async ({ msg, args }) => {
-    const embed = new MessageEmbed()
-      .setTitle('팀');
+    const embed = new MessageEmbed();
     if (!args[0])
       return msg.reply('기입할 이름을 입력해주세요');
     const length = args.length;
-    const team = shuffle(args);
-    for (let i = 0; i < length; i++) {
-      if (i < length / 2)
-        embed.addField('1팀', `${team[i]}`, false);
-      else
-        embed.addField('2팀', `${team[i]}`, false);
+    const shuffleArgs = shuffle(args);
+    const team1 = new Array();
+    const team2 = new Array();
+    for (let i = 0; i < length; i += 2) {
+      team1.push(shuffleArgs[i]);
+      team2.push(shuffleArgs[i + 1]);
     }
+
+    embed
+      .setTitle('팀')
+      .addFields(
+        { name: '1팀', value: `${team1.join(', ')}`, inline: false},
+        { name: '2팀', value: `${team2.join(', ')}`, inline: false},
+      );
 
     msg.channel.send({ embeds: [embed] });
   },

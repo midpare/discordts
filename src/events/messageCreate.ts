45ffebbb34884f9d64e8c9a40ed60a14f9ea2a09
midpare <midpare@gmail.com>
2022-01-19 11:39:55 +0900

    Update messageCreate.ts

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,7 +1,6 @@
 import { ExtendMessage } from '../typings/Command';
 import { client } from '../structures/Client';
 import { gambling } from '../models/gambling';
-import { dateCal } from '../structures/Util';
 
 export = {
   name: 'messageCreate',
@@ -13,17 +12,29 @@ export = {
     const [cmd, ...args] = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = client.mainCommands.get(cmd.toLowerCase());
     const aliase = client.mainAliases.get(cmd.toLowerCase());
-    const date = new Date();
-    const today = dateCal(date, 0);
+    const gambChannel1 = client.channels.cache.get('910521119877005367');
+    const gambChannel2 = client.channels.cache.get('915212166330736691');
+    const musicChannel = client.channels.cache.get('910521119877005366');
+    const cmdChannel = client.channels.cache.get('932162287224127520');
     try {
       if (command) {
-        if (command.category == 'gambling' && command.name != '가입') {
-          const user = await gambling.findOne({ id });
-          if (!user)
-            return msg.reply('가입되지 않은 유저입니다 !가입 을 통해 가입해주시기 바랍니다.');
+        switch (command.category) {
+          case '도박' || '코인' || '베팅':
+            if (msg.channel != gambChannel1 && msg.channel != gambChannel2)
+              return msg.reply('이 명령어는 도박방에서만 사용할 수 있습니다.');
 
-          if (user.bankruptcy && parseFloat(today) - user.bankruptcy < 3)
-            return msg.reply(`파산한지 3일이 지나지 않은 유저는 도박을 할 수 없습니다. 파산일${user.bankruptcy}`);
+            const user = await gambling.findOne({ id });
+            if (!user)
+              return msg.reply('가입되지 않은 유저입니다 !가입 을 통해 가입해주시기 바랍니다.');
+            break;
+          case '노래':
+            if (msg.channel != musicChannel)
+              return msg.reply('이 명령어는 노래방에서만 사용할 수 있습니다.');
+            break;
+          default:
+            if (msg.channel != cmdChannel)
+              return msg.reply('이 명령어는 명령어사용방에서만 사용할 수 있습니다.');
+            break;
         }
         command.execute({ msg, args });
       } else if (aliase) {

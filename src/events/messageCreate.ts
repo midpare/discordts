772e4a3290cbea2d7f39ac1ad69b49a14be74b09
midpare <midpare@gmail.com>
2022-01-21 17:53:39 +0900

    Update messageCreate.ts

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -6,7 +6,7 @@ export = {
   name: 'messageCreate',
   event: async (msg: ExtendMessage) => {
     const prefix = process.env.PREFIX || '';
-    if (msg.author.bot || msg.author.id === client.user.id || !msg.content.startsWith(prefix)) 
+    if (msg.author.bot || msg.author.id === client.user.id || !msg.content.startsWith(prefix))
       return;
 
     const id = msg.author.id;
@@ -18,40 +18,38 @@ export = {
     const cmdChannel = client.channels.cache.get('932162287224127520');
     const botTestChannel = client.channels.cache.get('910521119877005368');
     try {
-      if (command) {
-        if (command.stopping)
-          return msg.reply('이 명령어는 현재 개발자에 의해 정지돼있습니다.');
-          
-        if (msg.channel == botTestChannel)
-          command.execute({ msg, args });
-        else {
-          switch (command.category) {
-            case '도박':
-            case '베팅':
-            case '코인':
-              if (msg.channel != gambChannel1 && msg.channel != gambChannel2)
-                return msg.reply('이 명령어는 도박방에서만 사용할 수 있습니다.');
-
-              const user = await gambling.findOne({ id });
-              if (!user)
-                return msg.reply('가입되지 않은 유저입니다 !가입 을 통해 가입해주시기 바랍니다.');
-              break;
-            case '노래':
-              if (msg.channel != musicChannel)
-                return msg.reply('이 명령어는 노래방에서만 사용할 수 있습니다.');
-              break;
-            case '기본':
-            case '관리자':
-              command.execute({ msg, args });
-              break;
-            default:
-              if (msg.channel != cmdChannel)
-                return msg.reply('이 명령어는 명령어사용방에서만 사용할 수 있습니다.');
-              break;
-          }
-        }
-      } else
+      if (!command)
         return msg.reply(`정확한 명령어를 입력해주시기 바랍니다.\n${prefix}help`);
+
+      if (command.stopping)
+        return msg.reply('이 명령어는 현재 개발자에 의해 정지돼있습니다.');
+
+      if (msg.channel != botTestChannel) {
+        switch (command.category) {
+          case '도박':
+          case '베팅':
+          case '코인':
+            if (msg.channel != gambChannel1 && msg.channel != gambChannel2)
+              return msg.reply('이 명령어는 도박방에서만 사용할 수 있습니다.');
+
+            const user = await gambling.findOne({ id });
+            if (!user)
+              return msg.reply('가입되지 않은 유저입니다 !가입 을 통해 가입해주시기 바랍니다.');
+            break;
+          case '노래':
+            if (msg.channel != musicChannel)
+              return msg.reply('이 명령어는 노래방에서만 사용할 수 있습니다.');
+            break;
+          case '기본':
+          case '관리자':
+            break;
+          default:
+            if (msg.channel != cmdChannel)
+              return msg.reply('이 명령어는 명령어사용방에서만 사용할 수 있습니다.');
+            break;
+        }
+      }
+      command.execute({ msg, args });
     } catch (error) {
       console.error(error);
     }

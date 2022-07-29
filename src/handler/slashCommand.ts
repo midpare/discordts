4e10ea31c3d76c05a2ca48f3c3fd662542e28229
midpare <44808623+midpare@users.noreply.github.com>
2022-07-29 23:18:15 +0900

    update it

--- src/handler/slashCommand.ts
+++ src/handler/slashCommand.ts
@@ -1,20 +1,20 @@
-import { Client } from '../structures/Client';
-import { Utils } from '../structures/Utils';
+// import { Client } from '../structures/Client';
+// import { Utils } from '../structures/Utils';
 
-export default async function (client: Client) {
-  const slashCommandFiles = new Array();
-  Utils.getPath(__dirname + '/../slashCommands', slashCommandFiles);
+// export default async function (client: Client) {
+//   const slashCommandFiles = new Array();
+//   Utils.getPath(__dirname + '/../slashCommands', slashCommandFiles);
 
-  //Wait for bot to login 
-  setTimeout(async () => {
-    console.log(client.guilds.cache);
-    for (const path of slashCommandFiles) {
-      const file = (await import(path)).default;
+//   //Wait for bot to login 
+//   setTimeout(async () => {
+//     console.log(client.guilds.cache);
+//     for (const path of slashCommandFiles) {
+//       const file = (await import(path)).default;
 
-      for (const [_, guild] of client.guilds.cache) {
-        guild.commands.create(file);
-      }
-      client.slashCommand.set(file.name, file);
-    }
-  }, 2000);
-}
+//       for (const [_, guild] of client.guilds.cache) {
+//         guild.commands.create(file);
+//       }
+//       client.slashCommand.set(file.name, file);
+//     }
+//   }, 2000);
+// }

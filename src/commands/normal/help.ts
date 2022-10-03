f8109478c5125f578240e271ddf970b0de7b2171
midpare <midpare@gmail.com>
2022-10-03 11:42:40 +0900

    update it

--- src/commands/normal/help.ts
+++ src/commands/normal/help.ts
@@ -1,5 +1,7 @@
-import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { ActionRow, ActionRowBuilder, ApplicationCommandOptionType, Colors, EmbedBuilder, SelectMenuBuilder } from 'discord.js';
 import { Command } from '../../managers/Command';
+import { InteractionOption } from '../../structures/InteractionOptions';
+import { Utils } from '../../structures/Utils';
 
 export default new Command({
   name: 'help',
@@ -7,52 +9,84 @@ export default new Command({
   category: '기본',
   usage: 'help [카테고리]',
   description: '봇의 명령어를 확인합니다.',
-  options: [
-    {
-      name: '카테고리',
-      description: '명령어 카테고리를 입력합니다.',
-      type: ApplicationCommandOptionType.String,
-      required: false,
-    }
-  ],
-  execute: async ({ interaction, options, client }) => {
-    const embed = new EmbedBuilder();
+  execute: async ({ interaction, client }) => {
+    const { guildId, user: { id } } = interaction;
+
+    if (!guildId) 
+      return 0;
 
     const directories = [...new Set(client.commands.map(command => command.category).filter(category => category != undefined))];
+
+    const menuOptions = new Array();
     const categories = new Map();
+
     for (const category of directories) {
-      const getCommands = new Set(client.commands
+      const option = {
+        label: category,
+        description: `${category} 카테고리를 선택합니다.`,
+        value: category
+      }
+      menuOptions.push(option)
+
+      const commands = new Set(client.commands
         .filter(commands => commands.category == category)
-        .map(command => command.name)
+        .map(command => {
+          return {
+            name: command.usage,
+            description: command.description
+          }
+        })
       );
-      categories.set(category, [...getCommands]);
+      categories.set(category, commands)
     }
 
-    const category = options.getString('카테고리');
-    const commands = categories.get(category);
-    
-    if (!commands) {
-      embed
-        .setTitle('명령어')
-        .setDescription('명령어 카테고리를 확인합니다.');
-      for (const category of directories) {
-        embed
-          .addFields({ name: `/help ${category}`, value: `${category} 관련 명령어를 확인합니다.`, inline: false });
-      }
-      interaction.reply({ embeds: [embed] });
-    } else {
-      embed
-        .setTitle(`${category} 명령어`)
-        .setDescription(`${category} 관련 명령어를 확인합니다.\n<>는 필수, []는 선택사항 입니다.`);
-      for (const element of commands) {
-        const command = client.commands.get(element);
-        if (command) {
-          embed
-            .addFields({ name: '/' + command.usage, value: command.description, inline: false });
-        }
-      }
-      interaction.reply({ embeds: [embed] });
-    }
+    const customId = Utils.uuid()
+    const row = <ActionRowBuilder<SelectMenuBuilder>>new ActionRowBuilder().addComponents(
+      new SelectMenuBuilder()
+        .setCustomId(customId)
+        .setPlaceholder('이곳에서 카테고리를 선택하세요')
+        .setOptions(menuOptions)
+    )
+
+    const embed = new EmbedBuilder()
+      .setDescription('카테고리를 선택해주세요')
+      .setColor(Colors.Red);
+
+    interaction.reply({ embeds: [embed], components: [row] });
+
+    const msg = await interaction.fetchReply()
+    client.interactionOptions.set(customId, new InteractionOption({
+      ids: [id],
+      guildId,
+      cmd: 'help',
+      messages: [msg],
+      customIds: [customId],
+      data: {
+        categories
+      },
+    }));
+    // if (!commands) {
+    //   embed
+    //     .setTitle('명령어')
+    //     .setDescription('명령어 카테고리를 확인합니다.');
+    //   for (const category of directories) {
+    //     embed
+    //       .addFields({ name: `/help ${category}`, value: `${category} 관련 명령어를 확인합니다.`, inline: false });
+    //   }
+    //   interaction.reply({ embeds: [embed] });
+    // } else {
+    //   embed
+    //     .setTitle(`${category} 명령어`)
+    //     .setDescription(`${category} 관련 명령어를 확인합니다.\n<>는 필수, []는 선택사항 입니다.`);
+    //   for (const element of commands) {
+    //     const command = client.commands.get(element);
+    //     if (command) {
+    //       embed
+    //         .addFields({ name: '/' + command.usage, value: command.description, inline: false });
+    //     }
+    //   }
+    //   interaction.reply({ embeds: [embed] });
+    // }
     return 1;
   },
 });
---
+++ src/interactions/help.ts
@@ -0,0 +1,30 @@
+import { EmbedBuilder, SelectMenuInteraction } from 'discord.js';
+import { Interaction } from '../managers/Interaction';
+
+export default new Interaction<SelectMenuInteraction>({
+  name: 'help',
+  deleted: false,
+  execute: async ({ interaction, options, client }) => {
+    const category = interaction.values[0];
+    const commands = options?.data.categories.get(category);
+    
+    const fields = new Array();
+
+    for (const command of commands) {
+      fields.push({
+        name: '/' + command.name,
+        value: command.description,
+        inline: false,
+      });
+    }
+    const embed = new EmbedBuilder()
+      .setTitle(`${category} 명령어`)
+      .setDescription(`${category} 관련 명령어를 확인합니다.\n<>는 필수, []는 선택사항입니다.`)
+      .setFields(fields);
+
+    options?.messages[0].edit({ embeds: [embed] });
+    interaction.deferUpdate();
+
+    client.interactionOptions.set(interaction.customId, options!)
+  },
+});
